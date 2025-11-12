ae01c877b1a8ae8009c972152d53a4f4198c3c0f
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-11-12 23:30:17 +0600

    Initial commit of movie app

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        <h1 className="text-3xl font-bold text-center text-blue-600 mt-10">Movie App</h1>
+      <div>
+        <h1 className="text-3xl font-bold text-center text-blue-600 mt-10">
+          Movie App
+        </h1>
         <p>This is for Portfolio</p>
+      </div>
     </div>
-  )
+  );
 }
