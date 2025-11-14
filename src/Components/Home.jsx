5d3a39ba3f29db89cb88948a158f1e91b72d12b1
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-11-14 15:15:29 +0600

    Tailwind added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-        <h1>Movie App</h1>
+        <h1 className="text-3xl font-bold text-center text-blue-600 mt-10">Movie App</h1>
         <p>This is for Portfolio</p>
     </div>
   )
